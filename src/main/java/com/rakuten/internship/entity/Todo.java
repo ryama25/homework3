c1c4b1dd78bf0c2013935140e94a37c1038db852
RyotaYamada <ryotk3peso@gmail.com>
2019-08-29 16:06:28 +0000

    a

--- src/main/java/com/rakuten/internship/entity/Todo.java
+++ src/main/java/com/rakuten/internship/entity/Todo.java
@@ -4,6 +4,9 @@ package com.rakuten.internship.entity;
  * このクラスは、タスク管理の対象となるTodoのエンティティーです。
  * エンティティーとして使えるように、コードを記入してください。
  */
+@Entity
+@Data
 public class Todo {
     // TODO 必要なコードを記入してください。
+    
 }
